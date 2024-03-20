d4d321cd0544f41d9b3356b6833ca0e2762cd11c
Esra-333 <esra.omer444@gmail.com>
2024-03-20 01:41:06 -0700

    a

--- models/user.py
+++ models/user.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
@@ -11,4 +14,5 @@ class User(BaseModel):
     last_name = Column(String(128))
     places = relationship("Place", cascade='all, delete,
                            delete-orphan', backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan' backref="user")
+    reviews = relationship("Review", cascade='all, delete,
+                           delete-orphan' backref="user")
