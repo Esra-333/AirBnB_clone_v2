b9bfbf7057cb1bcc13299e8be68221c5035fab48
Esra-333 <esra.omer444@gmail.com>
2024-05-02 22:18:22 -0700

    a

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
@@ -13,10 +14,9 @@ def do_pack():
                                                          dt.minute
                                                          dt.secound)
 
-
-   if os.path.isdir("versions") is False:
+    if os.path.isdir("versions") is False:
        if local("mkdir -p versions").failed is True:
            return None
-   if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
        return None
-   return file
+    return file
