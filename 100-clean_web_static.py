e94d286debe8c1ac2ecc2edfecc9dacf612b60c7
Esra-333 <esra.omer444@gmail.com>
2024-05-03 17:19:43 -0700

    a

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,9 +9,8 @@ env.hosts = ["54.175.151.73", "100.25.129.78"]
 def do_clean(number=0):
     """Delete out-of-date archives.
     Args:
-    number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive.     if number is 2, keeps the most and second-most recent arc     hives,
-    etc
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent arc    hive.if number is 2, keeps the most and second-most    recent archives,etc
     """
     number = 1 if int(number) == 0 else int(number)
     archives = sorted(os.listdir("versions"))
